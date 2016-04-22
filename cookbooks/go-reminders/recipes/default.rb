b3c5835e04283a2dffe5879d1b0b326851c7e355
Tim Green <timsgreen@gmail.com>
2016-04-22 06:47:25 +0000

    include templates as part of the deployment

--- cookbooks/go-reminders/recipes/default.rb
+++ cookbooks/go-reminders/recipes/default.rb
@@ -23,6 +23,19 @@ artifactory_artifact "/usr/local/go-reminders/go-reminders" do
   mode "0755"
 end
 
+artifactory_artifact "/usr/local/go-reminders/html.tar.gz" do
+  artifactory_url "http://#{node['go-reminders']['artifactory']['server']}/artifactory"
+  artifactory_username node['go-reminders']['artifactory']['username']
+  artifactory_password node['go-reminders']['artifactory']['password']
+  repository "go-reminders"
+  repository_path "html.tar.gz"
+  mode "0755"
+end
+
+execute "tar" do
+  command "tar xvzf /usr/local/go-reminders/html.tar.gz -C /usr/local/go-reminders"
+end
+
 template "/etc/init/go-reminders.conf" do
   source "go-reminders.conf.erb"
   owner "root"
@@ -38,5 +51,5 @@ template "/etc/init/go-reminders.conf" do
 end
 
 service "go-reminders" do
-  action :start
+  action :nothing
 end
